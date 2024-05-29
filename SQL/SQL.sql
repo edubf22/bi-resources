8485c6298a6fffd6c67e23431c1d7056204a91b6
Edu Ferreira <97489435+edubf22@users.noreply.github.com>
2024-05-29 17:25:50 -0400

    Insert Alter Date Dimension View

--- SQL/SQL.sql
+++ SQL/SQL.sql
@@ -55,3 +55,71 @@ SELECT
     CAST(DATENAME(WEEKDAY, Calendar_Date) AS CHAR(10)) AS [DayName],
     CAST(DATENAME(WEEKDAY, Calendar_Date) AS CHAR(3)) AS [DayNameAbbr]
 FROM DateSequence;
+
+-- Alter a view (in this case, the date view above)
+ALTER VIEW [dbo].[dim_date_vw]
+AS
+/* view to create a date table based on the first and last date keys of the fact table */
+WITH E1(N) AS (
+    SELECT N FROM (VALUES (0),(1),(2),(3),(4),(5),(6),(7),(8),(9)) t(N)
+), E2(N) AS (
+    SELECT a.N FROM E1 a, E1 b -- 100 values
+), E4(N) AS (
+    SELECT a.N FROM E2 a, E2 b -- 10,000 values
+), cteDateRangeKeys AS (
+	SELECT 'start' = CAST( MIN( [DateKEY] ) AS CHAR(8) )
+	,'end' = CAST( MAX( [DateKEY] ) AS CHAR(8) )
+	FROM [dbo].[fact Sell-Through]
+), cteDateRange AS (
+	SELECT 'start' = DATEFROMPARTS( LEFT( [start], 4 ), SUBSTRING( [start], 5, 2 ), RIGHT( [start], 2 ) )
+	, 'end' = DATEFROMPARTS( LEFT( [end], 4 ), SUBSTRING( [end], 5, 2 ), RIGHT( [end],2 ) )
+	FROM cteDateRangeKeys
+), cteDates AS (
+	SELECT
+        'Date' = CAST([Calendar_Date] AS DATE)
+        ,'Year' = CAST(DATEPART(YEAR, [Calendar_Date]) AS INT)
+        ,'Quarter Number' = CAST(DATEPART(QUARTER, [Calendar_Date]) AS INT)
+        ,'Quarter Name' = CONCAT('Q', CAST(DATEPART(QUARTER, [Calendar_Date]) AS INT))
+        ,'Month Number' = CAST(DATEPART(MONTH, [Calendar_Date]) AS INT)
+        ,'Month Name' = CAST(DATENAME(MONTH, [Calendar_Date]) AS CHAR(10)) 
+        ,'Month Name Abbr' = CAST(DATENAME(MONTH, [Calendar_Date]) AS CHAR(3))
+        ,'Day of Month' = CAST(DATEPART(DAY, [Calendar_Date]) AS INT) 
+        ,'Day of Week' = CAST(DATEPART(DW, [Calendar_Date]) AS INT) 
+        ,'Day Name' = CAST(DATENAME(WEEKDAY, [Calendar_Date]) AS CHAR(10))
+        ,'Day Name Abbr' = CAST(DATENAME(WEEKDAY, [Calendar_Date]) AS CHAR(3))
+        ,'Fiscal Year' = CAST(DATEPART(YEAR, DATEADD(MONTH, 3, [Calendar_Date])) AS INT)
+        ,'Fiscal Quarter' = CAST((DATEPART(QUARTER, DATEADD(MONTH, 3, [Calendar_Date])) - 1) % 4 + 1 AS INT)
+        ,'Fiscal Month' = CAST((DATEPART(MONTH, DATEADD(MONTH, 3, [Calendar_Date])) - 1) % 12 + 1 AS INT)
+        ,'YearMonth' = CONCAT(CAST(DATENAME(MONTH, [Calendar_Date]) AS CHAR(3)), ' ', CAST(DATEPART(YEAR, [Calendar_Date]) AS CHAR(4)))
+	FROM (
+		SELECT 'Calendar_Date' = CAST( DATEADD(DAY, -1 + ROW_NUMBER() OVER ( ORDER BY (SELECT 1) ), dr.[start] ) AS DATE )
+		FROM E4, E1 /* creates a sequence of 1 - 100k */
+		CROSS JOIN cteDateRange dr 
+	) dteList
+	CROSS JOIN cteDateRange dr
+	WHERE dteList.[Calendar_Date] BETWEEN dr.[start] AND dr.[end] /* narrow it down to the fact table date range */
+
+) 
+
+SELECT [DateKEY] = CAST( YEAR( [Date] ) * 10000 + MONTH( [Date] ) * 100 + DAY( [Date] ) AS INT )
+, [Date]
+, [Year]
+, [Quarter Number]
+, [Quarter Name]
+, [Month Number]
+, [Month Name]
+, [Month Name Abbr]
+, [Day of Month]
+, [Day of Week]
+, [Day Name]
+, [Day Name Abbr]
+, [Fiscal Year]
+, [Fiscal Quarter]
+, [Fiscal Month]
+, [YearMonth]
+, 'Fiscal Year Name' = CONCAT('FY ', CAST([Fiscal Year] AS CHAR(4)))
+, 'Fiscal Quarter Name' = CONCAT('Q', CAST([Fiscal Quarter] AS CHAR(1)))
+FROM cteDates
+;
+GO
+

cf781bf41ae15499d7da241fe62b6ad62bb77071
Edu Ferreira <97489435+edubf22@users.noreply.github.com>
2024-06-14 12:43:37 -0400

    Create PySpark.py

---
+++ PySpark/PySpark.py
@@ -0,0 +1,27 @@
+# Select from a lakehouse table and write to a temporary view
+DeltaTableName = "<DeltaTableName>"
+
+df = spark.sql(f"""SELECT * FROM <LakehouseName>.{DeltaTableName}""")
+df.createOrReplaceTempView("<TempViewName>")
+
+df_formatted = spark.sql(f"""
+    SELECT 
+        <SourceTableField1>                     -- DestinationTableField name matches SourceTableField name
+        ,NULL AS <DestinationTableField>        -- Field is missing from SourceTable
+        ,<SourceTableField2>                    -- Another matching field, such as 'Year'
+        current_timestamp() AS CreatedOn,       -- CreatedOn for control by using a function
+    FROM <TempViewName>
+""")
+
+# Get the target table
+target_table = DeltaTable.forName(spark, "<LakehouseName>.<DestinationTableName>")
+    
+# Merge Data to Silver Layer
+target_table.alias("Target")\
+    .merge(
+        df_formatted.alias("Source"),
+        f"""Target.<MergeKey1> = Source.<MergeKey1> AND Target.<MergeKey2> = Source.<MergeKey2>""")\
+    .whenMatchedUpdateAll().whenNotMatchedInsertAll().execute()
+
+# Drop Temp Table
+spark.catalog.dropTempView("<TempViewName>")
